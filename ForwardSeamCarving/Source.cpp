b8ad802b4d76a6ae79f3676db46f9289fa55cd2a
Patiphol <patiphol.pus@student.mahidol.ac.th>
2019-03-29 22:39:10 +0700

    finish vertical seam line

--- ForwardSeamCarving/Source.cpp
+++ ForwardSeamCarving/Source.cpp
@@ -43,7 +43,7 @@ int main()
 		cout << grayimg << endl;
 
 		//Looping till get the command 'a', 'd', 'w', 's'
-		while (c != 97 && c != 100 && c != 119 && c != 27) {
+		while (c != 97 && c != 100 && c != 119 && c != 115) {
 			c = cvWaitKey(0);
 		}
 
@@ -54,13 +54,13 @@ int main()
 			// *** WRITE YOUR CODE ***
 			//CALCULATE M MATRIX
 			int x, y;
-			for (x = 0; x < grayimg_border.rows-2; x++)
+			for (x = 0; x < grayimg_border.rows - 2; x++)
 			{
-				for (y = 0; y < grayimg_border.cols-2; y++) {
+				for (y = 0; y < grayimg_border.cols - 2; y++) {
 					//cout << x << "|" << y << " ";
 					//if it's the first row > just calculate difference of left/right
 					if (x == 0) {
-						M.at<int>(x, y) = abs((int)grayimg_border.at<uchar>(x+1, y) - (int)grayimg_border.at<uchar>(x+1, y+2));
+						M.at<int>(x, y) = abs((int)grayimg_border.at<uchar>(x + 1, y) - (int)grayimg_border.at<uchar>(x + 1, y + 2));
 						// all elements in first row of K matrix are 0
 						K.at<int>(x, y) = 0;
 						//cout << M.at<int>(x, y) << " ";
@@ -73,7 +73,7 @@ int main()
 						int min;
 						//if y == 0 > no M @ CL > not considering CL to find min
 						if (y == 0) {
-							min = std::min({M.at<int>(x - 1, y) + cu, M.at<int>(x - 1, y + 1) + cr});
+							min = std::min({ M.at<int>(x - 1, y) + cu, M.at<int>(x - 1, y + 1) + cr });
 							if (min == (M.at<int>(x - 1, y) + cu)) {
 								K.at<int>(x, y) = 2;
 							}
@@ -83,7 +83,7 @@ int main()
 						}
 						//if y == last col > no M @ CR > not considering CR to find min
 						else if (y == grayimg_border.cols - 3) {
-							min = std::min({ M.at<int>(x - 1, y - 1) + cl, M.at<int>(x - 1, y) + cu});
+							min = std::min({ M.at<int>(x - 1, y - 1) + cl, M.at<int>(x - 1, y) + cu });
 							if (min == (M.at<int>(x - 1, y - 1) + cl)) {
 								K.at<int>(x, y) = 1;
 							}
@@ -115,21 +115,150 @@ int main()
 				//cout << endl;
 			}
 
-			cout << M << endl;
-			cout << K << endl;
+			//cout << M << endl;
+			//cout << K << endl;
 
 			//Find the best seam in the vertical direction
 			// *** WRITE YOUR CODE ***
 			tCol = (Mat_<int>(1, height));
 			int min = 999;
-			int heightCount = height-1;
-			cout << tCol;
+			int heightCount = height - 1;
+			//cout << tCol;
 			for (y = 0; y < width; y++) {
 				if (M.at<int>(height - 1, y) < min) {
 					min = M.at<int>(height - 1, y);
 					tCol.at<int>(0, heightCount) = y;
 				}
 			}
+			printf("Best seam is %d at index %d\n", min, tCol.at<int>(0, heightCount));
+			//cout << tCol << endl;
+
+			//printf("heightCount %d\n", heightCount);
+
+			//cout << K.at<int>(heightCount, tCol.at<int>(0, heightCount)) << endl;
+
+			while (heightCount != 0) {
+				if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 1) {
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount) - 1;
+				}
+				else if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 2) {
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount);
+				}
+				else if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 3) {
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount) + 1;
+				}
+				heightCount--;
+			}
+			//cout << tCol << endl;
+		}
+
+		//'w' => Increase height, 's' => reduce height
+		if (c == 115 || c == 119) {
+			//printf("119 or 115 is pressed\n");
+			//Construct M matrix and K matrix in horizontal direction
+			// *** WRITE YOUR CODE ***
+			/*Mat rotimg;
+			rotate(grayimg_border, rotimg, ROTATE_90_CLOCKWISE);
+			cout << "normal padding" << endl;
+			cout << grayimg_border << endl;
+			cout << "rotimg" << endl;
+			M = (Mat_<int>(rotimg.rows - 2, rotimg.cols - 2));
+			K = (Mat_<int>(rotimg.rows - 2, rotimg.cols - 2));
+			cout << "Rotimg Width " << rotimg.cols << " Height " << rotimg.rows << endl;
+			cout << "M Width" << M.cols << "Height" << M.rows << endl;
+			cout << "K Width" << K.cols << "Height" << K.rows << endl;
+			cout << rotimg << endl;*/
+
+			int x, y;
+			for (x = 0; x < grayimg_border.rows - 2; x++)
+			{
+				for (y = 0; y < grayimg_border.cols - 2; y++) {
+					printf("%d|%d|%d ", x, y, grayimg.at<uchar>(x,y));
+				}
+				printf("\n");
+			}
+			for (x = 0; x < grayimg_border.rows - 2; x++)
+			{
+				for (y = 0; y < grayimg_border.cols - 2; y++) {
+					//cout << x << "|" << y << " ";
+					//if it's the first row > just calculate difference of left/right
+					if (y == 0) {
+						M.at<int>(x, y) = abs((int)grayimg_border.at<uchar>(x, y+1) - (int)grayimg_border.at<uchar>(x + 2, y+1));
+						// all elements in first row of K matrix are 0
+						K.at<int>(x, y) = 0;
+						//cout << M.at<int>(x, y) << " ";
+					}
+					//else just calculate cl,cu,cr
+					else {
+						int cl = abs((int)grayimg_border.at<uchar>(x + 1, y) - (int)grayimg_border.at<uchar>(x + 2, y + 1)) + abs((int)grayimg_border.at<uchar>(x, y + 1) - (int)grayimg_border.at<uchar>(x + 2, y + 1));
+						int cu = abs((int)grayimg_border.at<uchar>(x, y + 1) - (int)grayimg_border.at<uchar>(x + 2, y + 1));
+						int cr = abs((int)grayimg_border.at<uchar>(x + 1 , y) - (int)grayimg_border.at<uchar>(x + 1, y + 1)) + abs((int)grayimg_border.at<uchar>(x, y + 1) - (int)grayimg_border.at<uchar>(x + 2, y + 1));
+						int min;
+						//if x == lastrow > no M @ CL > not considering CL to find min
+						if (x == height - 1) {
+							min = std::min({ M.at<int>(x, y - 1) + cu, M.at<int>(x - 1, y - 1) + cr });
+							if (min == (M.at<int>(x, y - 1) + cu)) {
+								/*printf("M is %d @ x:%d | y:%d\n", min, x,y);*/
+								K.at<int>(x, y) = 2;
+							}
+							else if (min == (M.at<int>(x - 1, y - 1) + cr)) {
+								K.at<int>(x, y) = 3;
+							}
+						}
+						//if y == last col > no M @ CR > not considering CR to find min
+						else if (x == 0) {
+							min = std::min({ M.at<int>(x + 1, y - 1) + cl, M.at<int>(x, y - 1) + cu });
+							if (min == (M.at<int>(x + 1, y - 1) + cl)) {
+								K.at<int>(x, y) = 1;
+							}
+							else if (min == (M.at<int>(x, y - 1) + cu)) {
+								K.at<int>(x, y) = 2;
+							}
+						}
+						//else just consider 3 things to find min
+						else {
+							min = std::min({ M.at<int>(x + 1, y - 1) + cl, M.at<int>(x, y - 1) + cu, M.at<int>(x - 1, y - 1) + cr });
+							if (min == (M.at<int>(x + 1, y - 1) + cl)) {
+								//if min = cl > k = 1
+								K.at<int>(x, y) = 1;
+							}
+							else if (min == (M.at<int>(x, y - 1) + cu)) {
+								//if min = cu > k = 2
+								K.at<int>(x, y) = 2;
+							}
+							else if (min == (M.at<int>(x - 1, y - 1) + cr)) {
+								//if min = cr > k = 3
+								K.at<int>(x, y) = 3;
+							}
+						}
+						M.at<int>(x, y) = min;
+						// all elements in first row of K matrix are 0
+						//cout << M.at<int>(x, y) << " ";
+					}
+				}
+				//cout << endl;
+			}
+
+			cout << M << endl;
+			cout << K << endl;
+
+			//Find the best seam in the horizontal direction
+			// *** WRITE YOUR CODE ***
+			tCol = (Mat_<int>(1, height));
+			int min = 999;
+			int heightCount = height - 1;
+			cout << tCol << endl;
+			cout << heightCount << endl;
+			cout << "Width " << width << " Height " << height << endl;
+			for (y = 0; y < height; y++) {
+				//printf("%d ", y);
+				if (M.at<int>(height - 1, y) < min) {
+					min = M.at<int>(height - 1, y);
+					tCol.at<int>(0, heightCount) = y;
+					cout << tCol << endl;
+				}
+			}
+			cout << tCol << endl;
 			printf("Best seam is %d at index %d\n", min, tCol.at<int>(0, heightCount));
 			cout << tCol << endl;
 
@@ -139,48 +268,117 @@ int main()
 
 			while (heightCount != 0) {
 				if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 1) {
-					tCol.at<int>(0, heightCount-1) = tCol.at<int>(0, heightCount) - 1;
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount) - 1;
 				}
 				else if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 2) {
-					tCol.at<int>(0, heightCount-1) = tCol.at<int>(0, heightCount);
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount);
 				}
 				else if (K.at<int>(heightCount, tCol.at<int>(0, heightCount)) == 3) {
-					tCol.at<int>(0, heightCount-1) = tCol.at<int>(0, heightCount) + 1;
+					tCol.at<int>(0, heightCount - 1) = tCol.at<int>(0, heightCount) + 1;
 				}
 				heightCount--;
 			}
 			cout << tCol << endl;
-		}
+			//rotate index of col to row
+			for (int i = 0; i < height; i++) {
+				tCol.at<int>(0, i) = abs(9 - tCol.at<int>(0, i));
+			}
+			cout << tCol << endl;
 
-		//'w' => Increase height, 's' => reduce height
-		if (c == 115 || c == 119) {
-			//Construct M matrix and K matrix in horizontal direction
-			// *** WRITE YOUR CODE ***
-
+			/*int x, y;
+			for (x = 0; x < grayimg_border.cols - 2; x++) {
+				for (y = 0; y < grayimg_border.rows - 2; y++) {
+					if (y == 0) {
+						M.at<int>(x, y) = abs((int)grayimg_border.at<uchar>(x, y + 1) - (int)grayimg_border.at<uchar>(x + 2, y + 1));
+						K.at<int>(x, y) = 0;
+					}
+					else {
+						int cl = abs((int)grayimg_border.at<uchar>(x, y) - (int)grayimg_border.at<uchar>(x + 1, y + 1)) + abs((int)grayimg_border.at<uchar>(x + 1, y + 1) - (int)grayimg_border.at<uchar>(x, y + 1));
+						int cu = abs((int)grayimg_border.at<uchar>(x, y + 1) - (int)grayimg_border.at<uchar>(x + 1, y));
+						int cr = abs((int)grayimg_border.at<uchar>(x, y + 2) - (int)grayimg_border.at<uchar>(x + 1, y)) + abs((int)grayimg_border.at<uchar>(x + 1, y + 2) - (int)grayimg_border.at<uchar>(x + 1, y));
+					}
+				}
+			}*/
 			//Find the best seam in the horizontal direction
 			// *** WRITE YOUR CODE ***
+
 		}
 
 		//Insert or delete the best seam
 		if (c == 97) {
 			//Reduce width or delete seam verticaly
 			//Copy the pixels into this image
-			Mat img_new(height, --width, CV_64FC3, Scalar(0, 0, 0));
+			Mat img_new(height, --width, CV_8UC3, Scalar(0, 0, 0));
 			// *** WRITE YOUR CODE ***
-			cout << tCol << endl;
+			//cout << tCol << endl;
+			int i, j;
+			bool shift = false;
+			printf("\n\n new image %d*%d \n", width, height);
+			for (i = 0; i < height; i++) {
+				int seamIndex = tCol.at<int>(0, i);
+				shift = false;
+				for (j = 0; j <= width; j++) {
+					if (j == seamIndex) {
+						shift = true;
+						continue;
+					}
+
+					if (shift == true) {
+						//cout << "SSS" << endl;
+						//printf("%d,%d ", i, j);
+						img_new.at<Vec3b>(i, j - 1) = img.at<Vec3b>(i, j);
+					}
+					else
+					{
+						//cout << img.at<Vec3b>(Point(i, j)) << endl;
+						//printf("%d|%d ", i, j);
+						img_new.at<Vec3b>(i, j) = img.at<Vec3b>(i, j);
+					}
+				}
+				//printf("\n");
+			}
 
 			//Show the resized image
 			imshow("Example", img_new);
 			//Clone img_new into ing for the nest loop processing
 			img.release();
-			//img = img_new.clone;
-			//img_new.release();
+			img = img_new.clone();
+			img_new.release();
 		}
 		if (c == 100) {
 			//Increase width or insert seam vertically
 			//Copy the pixels into this image
-			Mat img_new(height, ++width, CV_64FC3, Scalar(0, 0, 0));
+			Mat img_new(height, ++width, CV_8UC3, Scalar(0, 0, 0));
 			// *** WRITE YOUR CODE ***
+			int i, j;
+			bool shift = false;
+			printf("\n\n new image %d*%d \n", width, height);
+			for (i = 0; i < height; i++) {
+				int seamIndex = tCol.at<int>(0, i);
+				shift = false;
+				for (j = 0; j < width; j++) {
+					if (j == seamIndex) {
+						shift = true;
+						//printf("%d,%d ", i, j);
+						img_new.at<Vec3b>(i, j) = img.at<Vec3b>(i, j);
+						continue;
+
+					}
+
+					if (shift == true) {
+						//cout << "SSS" << endl;
+						//printf("%d,%d ", i, j);
+						img_new.at<Vec3b>(i, j) = img.at<Vec3b>(i, j-1);
+					}
+					else
+					{
+						//cout << img.at<Vec3b>(Point(i, j)) << endl;
+						//printf("%d|%d ", i, j);
+						img_new.at<Vec3b>(i, j) = img.at<Vec3b>(i, j);
+					}
+				}
+				printf("\n");
+			}
 
 			//Show the resized image
 			imshow("Example", img_new);
@@ -192,8 +390,29 @@ int main()
 		if (c == 115) {
 			//Reduce height or delete seam horizontally
 			//Copy the pixels into this image
-			Mat img_new(--height, width, CV_64FC3, Scalar(0, 0, 0));
+			printf("S is pressed");
+			Mat img_new(--height, width, CV_8UC3, Scalar(0, 0, 0));
 			// *** WRITE YOUR CODE ***
+			cout << tCol << endl;
+			int i, j;
+			bool shift = false;
+			printf("\n\n new image %d*%d \n", width, height);
+			cout << grayimg << endl;
+
+			for (j = 0; j < height; j++) {
+				for (i = 0; i < width; i++) {
+					int seamIndex = tCol.at<int>(0, i);
+					if (j == seamIndex) {
+						printf("%d|%d >> shift getting >> %d,%d\n", i, j,j+1,i);
+						img_new.at<Vec3b>(j, i) = img.at<Vec3b>(j+1, i);
+					}
+					else {
+						printf("%d|%d >> getting >> %d,%d\n", i, j, j, i);
+						img_new.at<Vec3b>(j, i) = img.at<Vec3b>(j, i);
+					}
+				}
+				printf("\n");
+			}
 
 			//Show the resized image
 			imshow("Example", img_new);
@@ -205,6 +424,7 @@ int main()
 		if (c == 119) {
 			//Increase height or insert seam horizontally
 			//Copy the pixels into this image
+			printf("W is pressed");
 			Mat img_new(++height, width, CV_64FC3, Scalar(0, 0, 0));
 			// *** WRITE YOUR CODE ***
 
